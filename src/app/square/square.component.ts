1a3129b590a28b88a3ceda718c34260b57d79312
originalsidd <originalsidd@gmail.com>
2023-05-09 14:00:39 +0530

    TicTacToe

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,7 +4,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button mat-raised-button>{{ value }}</button>
+      <button mat-raised-button *ngIf="!value">{{ value }}</button>
+      <button mat-raised-button color="red">{{ value }}</button>
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
