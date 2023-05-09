b87fc114c229b4a7dbcf44793f44de82e969de50
originalsidd <originalsidd@gmail.com>
2023-05-09 14:16:11 +0530

    Added colors

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,10 +4,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button mat-raised-button *ngIf="!value">{{ value }}</button>
-      <button mat-raised-button color="red">{{ value }}</button>
+      <button *ngIf="!value">{{ value }}</button>
+      <button style="background-color: #ee3333d0;" *ngIf="value == 'X'">{{ value }}</button>
+      <button style="background-color: #33ee33d0;" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; color: #fff;}']
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
